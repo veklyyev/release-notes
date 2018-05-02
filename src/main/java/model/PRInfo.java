71a3cf032eb8b9bf274b90e929088b725b1042fc
Yehen_veklyn@epam.com <1111111q>
2018-05-03 01:50:13 +0300

    add hashcode/equals for PRInfo

--- src/main/java/model/PRInfo.java
+++ src/main/java/model/PRInfo.java
@@ -22,4 +22,24 @@ public class PRInfo{
     public String getPRTitle() {
         return PRTitle;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PRInfo prInfo = (PRInfo) o;
+
+        if (assignee != null ? !assignee.equals(prInfo.assignee) : prInfo.assignee != null) return false;
+        if (status != null ? !status.equals(prInfo.status) : prInfo.status != null) return false;
+        return PRTitle != null ? PRTitle.equals(prInfo.PRTitle) : prInfo.PRTitle == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = assignee != null ? assignee.hashCode() : 0;
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (PRTitle != null ? PRTitle.hashCode() : 0);
+        return result;
+    }
 }
